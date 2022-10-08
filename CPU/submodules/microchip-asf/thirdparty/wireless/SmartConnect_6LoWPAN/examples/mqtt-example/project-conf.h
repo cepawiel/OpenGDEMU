448686053a9ca7bb6d6e9e2ee1b7949dbaa079b9
cepawiel <cepawiel@mtu.edu>
2022-10-08 16:27:29 -0500

    Remove third party asf files

--- CPU/submodules/microchip-asf/thirdparty/wireless/SmartConnect_6LoWPAN/examples/mqtt-example/project-conf.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROJECT_CONF_H_
-#define PROJECT_CONF_H_
-
-#define THSQ_CONF_NETSTACK      THSQ_CONF_NULLRDC
-//#define THSQ_CONF_NETSTACK      THSQ_CONF_DROWSIE
-
-#endif /* PROJECT_CONF_H_ */
-
--- CPU/submodules/microchip-asf/thirdparty/wireless/SmartConnect_6LoWPAN/services/ota_mgr/server/ota_mgr_server.h
+++
@@ -1,6 +0,0 @@
-#ifndef OTA_MGR_SERVER_H
-#define OTA_MGR_SERVER_H
-
-
-
-#endif /* OTA_MGR_SERVER_H */
